211e8896b64e18a35ed6df186663cbc632b85044
SerG <serg-sv13@mail.ru>
2022-10-19 20:22:27 +0300

    Задача вывода чётных чисел

--- DZ1_C#/Task_8/Program.cs
+++ DZ1_C#/Task_8/Program.cs
@@ -3,4 +3,24 @@
 //      5 -> 2, 4
 //      8 -> 2, 4, 6, 8
 
-Console.WriteLine("Hello, World!");
+int temp = 1;
+bool not = true;
+
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Чётные числа от 1 до " + number);
+while (temp <= number)
+    {
+        if (temp % 2 != 1)
+            {
+                Console.Write(temp + ", ");
+                not = false;
+            }
+            temp++;
+    }
+
+        if (not)
+        {
+            Console.WriteLine("Нет чётных чисел!");
+        }
