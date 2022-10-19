3839ea3887e21369da9f6743d13792aa26c65648
SerG <serg-sv13@mail.ru>
2022-10-19 18:49:55 +0300

    Задача на нахождение большего числа из трёх

--- DZ1_C#/Task_4/Program.cs
+++ DZ1_C#/Task_4/Program.cs
@@ -5,4 +5,24 @@
 //      22 3 9 -> 22
 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Программа нахождения максимального числа из трёх");
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine()!);
+
+int max = number1;
+
+if (number2 > max)
+    {
+       max = number2;
+     }
+if (number3 > max)
+    {
+        max = number3;
+    }
+
+Console.WriteLine("Наибольшее из введённых чисел:" + max);
