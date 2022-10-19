9543bf57dc03fe90e9a939af3b3639bbb70e06de
SerG <serg-sv13@mail.ru>
2022-10-19 18:25:42 +0300

    Задача нахождения большего числа

--- DZ1_C#/Task_2/Program.cs
+++ DZ1_C#/Task_2/Program.cs
@@ -4,4 +4,25 @@
 //      a = 2 b = 10 -> max = 10
 //      a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Программа для вывода большего числа издвух");
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 == number2)
+    {
+        Console.WriteLine("Введены одинаковые числа");
+        }
+else 
+    {
+        if (number1 > number2)
+    {
+        Console.WriteLine("Первое число " + number1 + " больше чем второе " + number2);
+        }
+else
+    {
+        Console.WriteLine("Второе число " + number2 + " больше чем первое " + number1);
+        }
+        }
